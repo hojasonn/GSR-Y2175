2760b4b73567200f4ed9db399a78842587458a47
Jason Ho <jnh201@campus.usask.ca>
2018-11-23 17:12:00 -0600

    Running Y2175 Holder Test Error Propogation (PLATO_HolderTestPlots-s0optErrorv1.nb) on plato.usask.ca on 2018-11-23

--- HolderTest/evalMMA.m
+++ HolderTest/evalMMA.m
@@ -1,6 +1,6 @@
 Begin["EvalContext`"]
 
-nbFileName = FileNameJoin[{"/home/jnh201/GSR-y2175/HolderTest", "2018-11-21_HolderTestPlots-1--.nb"}];
+nbFileName = FileNameJoin[{"/home/jnh201/GSR-y2175/HolderTest/", "PLATO_HolderTestPlots-s0optErrorv1.nb"}];
 
 outputCellPattern = Cell[_, "Output", ___];
 
